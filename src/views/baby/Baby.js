3399581851d0650a7e7741ef174df8e0ce652531
zhaomingfang <15000763458@163.com>
2019-06-22 15:20:59 +0800

    state

--- src/views/baby/Baby.js
+++ src/views/baby/Baby.js
@@ -1,10 +1,26 @@
-import React from 'react'
-class Baby extends React.Component{
-    render(){
-        return(
-            <h1> 我是Baby</h1>
-        )
+import { connect } from 'react-redux';
+import UI from './UI';
 
-    }
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    height: state.heightlist,
+    touxiang: state.touxiang,
+    weight: state.weight,
+    age: state.age,
+    name: state.name
+  }
 }
-export default Baby
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getBannerList () {
+      fetch('http://www.daxunxun.com/banner').then(res => res.json()).then(data => {
+        dispatch({
+          type: 'changeBannerList',
+          data
+        })
+      })
+    },
+  }
+}
