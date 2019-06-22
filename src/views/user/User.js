38f5295ea773f14e2136941fe6f7932bda9c7c74
WYJTang <643392718@qq.com>
2019-06-22 14:25:55 +0800

    kings

--- src/views/user/User.js
+++ src/views/user/User.js
@@ -1,10 +1,15 @@
 import React from 'react'
-class User extends React.Component{
-    render(){
-        return(
-            <h1> 我是个人中心</h1>
-        )
-
+import axios from 'axios'
+class User extends React.Component {
+    state = {
+        list: []
+    }
+    componentDidMount(){
+    }
+    render() {
+            return(
+                <div>个人中心</div>
+                )
     }
 }
 export default User
